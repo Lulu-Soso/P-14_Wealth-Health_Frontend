c91898a80c3cd0b29503176eddf0a0d12ddff927
Lulu-Soso <lucas.sokha@gmail.com>
2023-09-08 17:07:22 +0200

    modification table

--- src/pages/EmployeesListPage.jsx
+++ src/pages/EmployeesListPage.jsx
@@ -7,12 +7,39 @@ import {
   setError,
   setSearch,
 } from "../feature/employees.slice";
-import { FaCaretUp, FaCaretDown } from "react-icons/fa6";
+import TableHeader from "../components/TableHeader";
+import EmployeeDataRow from "../components/EmployeeDataRow";
+import ShowEntries from "../components/FilterEntries";
+import SearchField from "../components/SearchField";
+import EntriesInfo from "../components/EntriesInfo";
+import Paginate from "../components/Paginate";
+
+// *** CONSTANTS ***
+const columns = [
+  { key: "firstName", label: "First Name" },
+  { key: "lastName", label: "Last Name" },
+  { key: "birthDate", label: "Date of Birth" },
+  { key: "startDate", label: "Start Date" },
+  { key: "street", label: "Street" },
+  { key: "city", label: "City" },
+  { key: "state", label: "State" },
+  { key: "zipCode", label: "Zip Code" },
+  { key: "department", label: "Department" },
+];
 
 const EmployeesListPage = () => {
+  // *** STATES ***
+  const [showEmptySearch, setShowEmptySearch] = useState(false);
+  const [sortBy, setSortBy] = useState("firstName");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [entriesToShow, setEntriesToShow] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchValue, setSearchValue] = useState("");
+
   const employeesData = useSelector((state) => state.employees.employeesData);
   const dispatch = useDispatch();
 
+  // *** DATA FETCHING ***
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -27,24 +54,10 @@ const EmployeesListPage = () => {
     fetchData();
   }, [dispatch]);
 
-  const [sortBy, setSortBy] = useState("firstName");
-  const [sortOrder, setSortOrder] = useState("asc");
-
+  // *** SORTING LOGIC ***
   const toggleSortOrder = () => {
     setSortOrder(sortOrder === "asc" ? "desc" : "asc");
   };
-
-  const sortedData = employeesData.slice().sort((a, b) => {
-    if (sortBy === null) return 0;
-
-    const aValue = a[sortBy];
-    const bValue = b[sortBy];
-    if (sortOrder === "asc") {
-      return aValue.localeCompare(bValue);
-    } else {
-      return bValue.localeCompare(aValue);
-    }
-  });
 
   const handleColumnClick = (columnName) => {
     if (sortBy === columnName) {
@@ -55,40 +68,30 @@ const EmployeesListPage = () => {
     }
   };
 
-  const [entriesToShow, setEntriesToShow] = useState(10);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [searchValue, setSearchValue] = useState("");
+  const sortedData = employeesData.slice().sort((a, b) => {
+    if (sortBy === null) return 0;
 
+    const aValue = a[sortBy];
+    const bValue = b[sortBy];
+
+    // Check for undefined or null values
+    if (aValue == null || bValue == null) return 0;
+
+    if (typeof aValue === "string" && typeof bValue === "string") {
+      if (sortOrder === "asc") {
+        return aValue.localeCompare(bValue);
+      } else {
+        return bValue.localeCompare(aValue);
+      }
+    } else {
+      // Handle non-string values, assuming they're numbers or other sortable types
+      return sortOrder === "asc" ? aValue - bValue : bValue - aValue;
+    }
+  });
+
+  // *** PAGINATION LOGIC ***
   const totalEntries = sortedData.length;
   const totalPages = Math.ceil(totalEntries / entriesToShow);
-
-  const paginatedData = sortedData
-    .filter((employee) => {
-      if (!searchValue) return true;
-      return (
-        employee.firstName.toLowerCase().includes(searchValue.toLowerCase()) ||
-        employee.lastName.toLowerCase().includes(searchValue.toLowerCase()) ||
-        employee.birthDate.toLowerCase().includes(searchValue.toLowerCase()) ||
-        employee.startDate.toLowerCase().includes(searchValue.toLowerCase()) ||
-        employee.street.toLowerCase().includes(searchValue.toLowerCase()) ||
-        employee.city.toLowerCase().includes(searchValue.toLowerCase()) ||
-        employee.state.toLowerCase().includes(searchValue.toLowerCase()) ||
-        employee.zipCode.toLowerCase().includes(searchValue.toLowerCase()) ||
-        employee.department.toLowerCase().includes(searchValue.toLowerCase())
-      );
-    })
-    .slice((currentPage - 1) * entriesToShow, currentPage * entriesToShow);
-
-  const handleSearchChange = (e) => {
-    setSearchValue(e.target.value);
-    setCurrentPage(1); // Reset to page 1 on search
-    dispatch(setSearch(e.target.value));
-  };
-
-  const handleEntriesChange = (e) => {
-    setEntriesToShow(+e.target.value);
-    setCurrentPage(1); // Reset to page 1 on entries change
-  };
 
   const handlePreviousPage = () => {
     if (currentPage > 1) setCurrentPage(currentPage - 1);
@@ -140,326 +143,106 @@ const EmployeesListPage = () => {
     return pages;
   }
 
+  // *** SEARCH AND FILTER LOGIC ***
+  const paginatedData = sortedData
+    .filter((employee) => {
+      if (!searchValue) return true;
+      return (
+        employee.firstName.toLowerCase().includes(searchValue.toLowerCase()) ||
+        employee.lastName.toLowerCase().includes(searchValue.toLowerCase()) ||
+        employee.birthDate.toLowerCase().includes(searchValue.toLowerCase()) ||
+        employee.startDate.toLowerCase().includes(searchValue.toLowerCase()) ||
+        employee.street.toLowerCase().includes(searchValue.toLowerCase()) ||
+        employee.city.toLowerCase().includes(searchValue.toLowerCase()) ||
+        employee.state.toLowerCase().includes(searchValue.toLowerCase()) ||
+        employee.zipCode.toLowerCase().includes(searchValue.toLowerCase()) ||
+        employee.department.toLowerCase().includes(searchValue.toLowerCase())
+      );
+    })
+    .slice((currentPage - 1) * entriesToShow, currentPage * entriesToShow);
+
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+    setCurrentPage(1); // Reset to page 1 on search
+    dispatch(setSearch(e.target.value));
+  };
+
+  const handleEntriesChange = (e) => {
+    setEntriesToShow(+e.target.value);
+    setCurrentPage(1); // Reset to page 1 on entries change
+  };
+
+  useEffect(() => {
+    if (paginatedData.length === 0) {
+      setShowEmptySearch(true);
+    } else {
+      setShowEmptySearch(false);
+    }
+  }, [paginatedData]);
+
   return (
     <div className="app-container">
       <div className="employees-header">
         <h2>Current Employees</h2>
         <div className="show-search">
-          <div className="filter">
-            <label htmlFor="show">Show</label>
-            <select
-              name="state"
-              id="show"
-              value={entriesToShow}
-              onChange={handleEntriesChange}
-            >
-              <option value="10">10</option> {/* Corrected values */}
-              <option value="25">25</option>
-              <option value="50">50</option>
-              <option value="100">100</option>
-            </select>
-            <p>entries</p>
-          </div>
-          <div className="search">
-            <label htmlFor="search">Search:</label>
-            <input
-              id="search"
-              type="text"
-              value={searchValue}
-              onChange={handleSearchChange}
-            />
-          </div>
+          <ShowEntries
+            entriesToShow={entriesToShow}
+            handleEntriesChange={handleEntriesChange}
+          />
+
+          <SearchField
+            searchValue={searchValue}
+            handleSearchChange={handleSearchChange}
+          />
         </div>
       </div>
 
       <table className="employees-table">
         <thead>
           <tr>
-            <th
-              onClick={() => handleColumnClick("firstName")}
-              className={`${sortBy === "firstName" ? "sorted-column" : ""}`}
-            >
-              <p className="flex-jcc-aic">
-                First Name
-                <span className="up-down">
-                  <FaCaretUp
-                    className={
-                      sortBy === "firstName" && sortOrder === "asc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                  <FaCaretDown
-                    className={
-                      sortBy === "firstName" && sortOrder === "desc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                </span>
-              </p>
-            </th>
-            <th
-              onClick={() => handleColumnClick("lastName")}
-              className={`${sortBy === "lastName" ? "sorted-column" : ""}`}
-            >
-              <p className="flex-jcc-aic">
-                Last Name
-                <span className="up-down">
-                  <FaCaretUp
-                    className={
-                      sortBy === "lastName" && sortOrder === "asc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                  <FaCaretDown
-                    className={
-                      sortBy === "lastName" && sortOrder === "desc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                </span>
-              </p>
-            </th>
-            <th
-              onClick={() => handleColumnClick("birthDate")}
-              className={`${sortBy === "birthDate" ? "sorted-column" : ""}`}
-            >
-              <p className="flex-jcc-aic">
-                Date of Birth
-                <span className="up-down">
-                  <FaCaretUp
-                    className={
-                      sortBy === "birthDate" && sortOrder === "asc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                  <FaCaretDown
-                    className={
-                      sortBy === "birthDate" && sortOrder === "desc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                </span>
-              </p>
-            </th>
-            <th
-              onClick={() => handleColumnClick("startDate")}
-              className={`${sortBy === "startDate" ? "sorted-column" : ""}`}
-            >
-              <p className="flex-jcc-aic">
-                Start Date
-                <span className="up-down">
-                  <FaCaretUp
-                    className={
-                      sortBy === "startDate" && sortOrder === "asc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                  <FaCaretDown
-                    className={
-                      sortBy === "startDate" && sortOrder === "desc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                </span>
-              </p>
-            </th>
-            <th
-              onClick={() => handleColumnClick("street")}
-              className={`${sortBy === "street" ? "sorted-column" : ""}`}
-            >
-              <p className="flex-jcc-aic">
-                Street
-                <span className="up-down">
-                  <FaCaretUp
-                    className={
-                      sortBy === "street" && sortOrder === "asc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                  <FaCaretDown
-                    className={
-                      sortBy === "street" && sortOrder === "desc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                </span>
-              </p>
-            </th>
-            <th
-              onClick={() => handleColumnClick("city")}
-              className={`${sortBy === "city" ? "sorted-column" : ""}`}
-            >
-              <p className="flex-jcc-aic">
-                City
-                <span className="up-down">
-                  <FaCaretUp
-                    className={
-                      sortBy === "city" && sortOrder === "asc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                  <FaCaretDown
-                    className={
-                      sortBy === "city" && sortOrder === "desc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                </span>
-              </p>
-            </th>
-            <th
-              onClick={() => handleColumnClick("state")}
-              className={`${sortBy === "state" ? "sorted-column" : ""}`}
-            >
-              <p className="flex-jcc-aic">
-                State
-                <span className="up-down">
-                  <FaCaretUp
-                    className={
-                      sortBy === "state" && sortOrder === "asc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                  <FaCaretDown
-                    className={
-                      sortBy === "state" && sortOrder === "desc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                </span>
-              </p>
-            </th>
-            <th
-              onClick={() => handleColumnClick("zipCode")}
-              className={`${sortBy === "zipCode" ? "sorted-column" : ""}`}
-            >
-              <p className="flex-jcc-aic">
-                Zip Code
-                <span className="up-down">
-                  <FaCaretUp
-                    className={
-                      sortBy === "zipCode" && sortOrder === "asc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                  <FaCaretDown
-                    className={
-                      sortBy === "zipCode" && sortOrder === "desc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                </span>
-              </p>
-            </th>
-            <th
-              onClick={() => handleColumnClick("department")}
-              className={`${sortBy === "department" ? "sorted-column" : ""}`}
-            >
-              <p className="flex-jcc-aic">
-                Department
-                <span className="up-down">
-                  <FaCaretUp
-                    className={
-                      sortBy === "department" && sortOrder === "asc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                  <FaCaretDown
-                    className={
-                      sortBy === "department" && sortOrder === "desc"
-                        ? "sorted-icon"
-                        : ""
-                    }
-                  />
-                </span>
-              </p>
-            </th>
+            {columns.map((column) => (
+              <TableHeader
+                key={column.key}
+                column={column}
+                sortBy={sortBy}
+                sortOrder={sortOrder}
+                onClick={() => handleColumnClick(column.key)} // Ici, on passe le nom de la colonne
+              />
+            ))}
           </tr>
         </thead>
         <tbody>
-          {paginatedData?.map((employee, index) => (
-            <tr
+          {paginatedData.map((employee, index) => (
+            <EmployeeDataRow
               key={employee.id}
+              employee={employee}
+              sortBy={sortBy}
               className={index % 2 === 0 ? "table-row-even" : "table-row-odd"}
-            >
-              <td className={sortBy === "firstName" ? "sorted-column" : ""}>
-                {employee.firstName}
-              </td>
-              <td className={sortBy === "lastName" ? "sorted-column" : ""}>
-                {employee.lastName}
-              </td>
-              <td className={sortBy === "birthDate" ? "sorted-column" : ""}>
-                {employee.birthDate}
-              </td>
-              <td className={sortBy === "startDate" ? "sorted-column" : ""}>
-                {employee.startDate}
-              </td>
-              <td className={sortBy === "street" ? "sorted-column" : ""}>
-                {employee.street}
-              </td>
-              <td className={sortBy === "city" ? "sorted-column" : ""}>
-                {employee.city}
-              </td>
-              <td className={sortBy === "state" ? "sorted-column" : ""}>
-                {employee.state}
-              </td>
-              <td className={sortBy === "zipCode" ? "sorted-column" : ""}>
-                {employee.zipCode}
-              </td>
-              <td className={sortBy === "department" ? "sorted-column" : ""}>
-                {employee.department}
-              </td>
-            </tr>
+            />
           ))}
         </tbody>
       </table>
+
+      {showEmptySearch && (
+        <div className="error-message">
+          <p>No results found for your search.</p>
+        </div>
+      )}
+
       <div className="flex-pagination">
-        <div>
-          Showing {(currentPage - 1) * entriesToShow + 1} to{" "}
-          {Math.min(currentPage * entriesToShow, totalEntries)} of{" "}
-          {totalEntries} entries
-        </div>
+        <EntriesInfo
+          currentPage={currentPage}
+          entriesToShow={entriesToShow}
+          totalEntries={totalEntries}
+        />
 
-        <div className="pagination">
-          <button className="previous-next" onClick={handlePreviousPage}>
-            Previous
-          </button>
-
-          {pageNumbers.map((number, index) => (
-            <React.Fragment key={number}>
-              {index > 0 && pageNumbers[index - 1] !== number - 1 && (
-                <span>...</span>
-              )}
-              <button
-                className={number === currentPage ? "current-page" : ""}
-                onClick={() => handlePageClick(number)}
-              >
-                {number}
-              </button>
-            </React.Fragment>
-          ))}
-
-          <button className="previous-next" onClick={handleNextPage}>
-            Next
-          </button>
-        </div>
+        <Paginate
+          handlePageClick={handlePageClick}
+          handlePreviousPage={handlePreviousPage}
+          handleNextPage={handleNextPage}
+          pageNumbers={pageNumbers}
+          currentPage={currentPage}
+        />
       </div>
       <div className="link-employee">
         <Link to="/employees/create">Create Employee</Link>
